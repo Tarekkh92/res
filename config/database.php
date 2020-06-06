fe44a25bcd6f83ca599a6abf5a472b54c701d45c
tarek khalaily <khalailyt@gmail.com>
2020-06-06 10:50:14 +0300

    Update database connection

--- config/database.php
+++ config/database.php
@@ -1,4 +1,6 @@
 <?php
+$DATABASE_URL=parse_url('postgres://pstwyihihckxoc:2b00a108003d83add40fe5fe065d814aeaf9f0785a01cc337ed0f37986df530b@ec2-34-200-72-77.compute-1.amazonaws.com:5432/d5flgms4d8it51');
+
 
 return [
 
@@ -13,7 +15,9 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
+
+    
 
     /*
     |--------------------------------------------------------------------------
@@ -56,13 +60,14 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"],"/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
+            'prefix_indexes'=>true,
             'schema' => 'public',
             'sslmode' => 'prefer',
         ],
